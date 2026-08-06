f512df18a2194afa2c447b56ee37b6cfa758cf49
F7NXPWL <dean.mazboudi@fiserv.com>
2026-08-06 12:23:42 -0400

    drive mapper sample psadt

---
+++ RefactorApps/samplePSADT/FiservDriveMapper-Deploy-Application.ps1
@@ -0,0 +1,368 @@
+﻿<#
+.SYNOPSIS
+
+PSApppDeployToolkit - This script performs the installation or uninstallation of an application(s).
+
+.DESCRIPTION
+
+- The script is provided as a template to perform an install or uninstall of an application(s).
+- The script either performs an "Install" deployment type or an "Uninstall" deployment type.
+- The install deployment type is broken down into 3 main sections/phases: Pre-Install, Install, and Post-Install.
+
+The script dot-sources the AppDeployToolkitMain.ps1 script which contains the logic and functions required to install or uninstall an application.
+
+PSApppDeployToolkit is licensed under the GNU LGPLv3 License - (C) 2023 PSAppDeployToolkit Team (Sean Lillis, Dan Cunningham and Muhammad Mashwani).
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the
+Free Software Foundation, either version 3 of the License, or any later version. This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+for more details. You should have received a copy of the GNU Lesser General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+.PARAMETER DeploymentType
+
+The type of deployment to perform. Default is: Install.
+
+.PARAMETER DeployMode
+
+Specifies whether the installation should be run in Interactive, Silent, or NonInteractive mode. Default is: Interactive. Options: Interactive = Shows dialogs, Silent = No dialogs, NonInteractive = Very silent, i.e. no blocking apps. NonInteractive mode is automatically set if it is detected that the process is not user interactive.
+
+.PARAMETER AllowRebootPassThru
+
+Allows the 3010 return code (requires restart) to be passed back to the parent process (e.g. SCCM) if detected from an installation. If 3010 is passed back to SCCM, a reboot prompt will be triggered.
+
+.PARAMETER TerminalServerMode
+
+Changes to "user install mode" and back to "user execute mode" for installing/uninstalling applications for Remote Desktop Session Hosts/Citrix servers.
+
+.PARAMETER DisableLogging
+
+Disables logging to file for the script. Default is: $false.
+
+.EXAMPLE
+
+powershell.exe -Command "& { & '.\Deploy-Application.ps1' -DeployMode 'Silent'; Exit $LastExitCode }"
+
+.EXAMPLE
+
+powershell.exe -Command "& { & '.\Deploy-Application.ps1' -AllowRebootPassThru; Exit $LastExitCode }"
+
+.EXAMPLE
+
+powershell.exe -Command "& { & '.\Deploy-Application.ps1' -DeploymentType 'Uninstall'; Exit $LastExitCode }"
+
+.EXAMPLE
+
+Deploy-Application.exe -DeploymentType "Install" -DeployMode "Silent"
+
+.INPUTS
+
+None
+
+You cannot pipe objects to this script.
+
+.OUTPUTS
+
+None
+
+This script does not generate any output.
+
+.NOTES
+
+Toolkit Exit Code Ranges:
+- 60000 - 68999: Reserved for built-in exit codes in Deploy-Application.ps1, Deploy-Application.exe, and AppDeployToolkitMain.ps1
+- 69000 - 69999: Recommended for user customized exit codes in Deploy-Application.ps1
+- 70000 - 79999: Recommended for user customized exit codes in AppDeployToolkitExtensions.ps1
+
+.LINK
+
+https://psappdeploytoolkit.com
+#>
+
+
+[CmdletBinding()]
+Param (
+    [Parameter(Mandatory = $false)]
+    [ValidateSet('Install', 'Uninstall', 'Repair')]
+    [String]$DeploymentType = 'Install',
+    [Parameter(Mandatory = $false)]
+    [ValidateSet('Interactive', 'Silent', 'NonInteractive')]
+    [String]$DeployMode = 'Interactive',
+    [Parameter(Mandatory = $false)]
+    [switch]$AllowRebootPassThru = $false,
+    [Parameter(Mandatory = $false)]
+    [switch]$TerminalServerMode = $false,
+    [Parameter(Mandatory = $false)]
+    [switch]$DisableLogging = $false
+)
+
+Try {
+    ## Set the script execution policy for this process
+    Try {
+        Set-ExecutionPolicy -ExecutionPolicy 'ByPass' -Scope 'Process' -Force -ErrorAction 'Stop'
+    }
+    Catch {
+    }
+
+    ##*===============================================
+    ##* VARIABLE DECLARATION
+    ##*===============================================
+    ## Variables: Application
+    [String]$appVendor = 'Fiserv'
+    [String]$appName = 'Drive Mapper'
+    [String]$appVersion = '1.0'
+    [String]$appArch = 'x64'
+    [String]$appLang = 'EN'
+    [String]$appRevision = '01'
+    [String]$appScriptVersion = '1.0.0'
+    [String]$appScriptDate = '09/26/2024'
+    [String]$appScriptAuthor = 'Joe Cassera'
+    ##*===============================================
+    ## Variables: Install Titles (Only set here to override defaults set by the toolkit)
+    [String]$installName = 'Fiserv Drive Mapper'
+    [String]$installTitle = 'Fiserv Drive Mapper'
+
+    ##* Do not modify section below
+    #region DoNotModify
+
+    ## Variables: Exit Code
+    [Int32]$mainExitCode = 0
+
+    ## Variables: Script
+    [String]$deployAppScriptFriendlyName = 'Deploy Application'
+    [Version]$deployAppScriptVersion = [Version]'3.9.3'
+    [String]$deployAppScriptDate = '02/05/2023'
+    [Hashtable]$deployAppScriptParameters = $PsBoundParameters
+
+    ## Variables: Environment
+    If (Test-Path -LiteralPath 'variable:HostInvocation') {
+        $InvocationInfo = $HostInvocation
+    }
+    Else {
+        $InvocationInfo = $MyInvocation
+    }
+    [String]$scriptDirectory = Split-Path -Path $InvocationInfo.MyCommand.Definition -Parent
+
+    ## Dot source the required App Deploy Toolkit Functions
+    Try {
+        [String]$moduleAppDeployToolkitMain = "$scriptDirectory\AppDeployToolkit\AppDeployToolkitMain.ps1"
+        If (-not (Test-Path -LiteralPath $moduleAppDeployToolkitMain -PathType 'Leaf')) {
+            Throw "Module does not exist at the specified location [$moduleAppDeployToolkitMain]."
+        }
+        If ($DisableLogging) {
+            . $moduleAppDeployToolkitMain -DisableLogging
+        }
+        Else {
+            . $moduleAppDeployToolkitMain
+        }
+    }
+    Catch {
+        If ($mainExitCode -eq 0) {
+            [Int32]$mainExitCode = 60008
+        }
+        Write-Error -Message "Module [$moduleAppDeployToolkitMain] failed to load: `n$($_.Exception.Message)`n `n$($_.InvocationInfo.PositionMessage)" -ErrorAction 'Continue'
+        ## Exit the script, returning the exit code to SCCM
+        If (Test-Path -LiteralPath 'variable:HostInvocation') {
+            $script:ExitCode = $mainExitCode; Exit
+        }
+        Else {
+            Exit $mainExitCode
+        }
+    }
+
+    #endregion
+    ##* Do not modify section above
+    ##*===============================================
+    ##* END VARIABLE DECLARATION
+    ##*===============================================
+
+    If ($deploymentType -ine 'Uninstall' -and $deploymentType -ine 'Repair') {
+        ##*===============================================
+        ##* PRE-INSTALLATION
+        ##*===============================================
+        [String]$installPhase = 'Pre-Installation'
+
+        ## Show Welcome Message, close Internet Explorer if required, allow up to 3 deferrals, verify there is enough disk space to complete the install, and persist the prompt
+        #Show-InstallationWelcome -CloseApps 'iexplore' -AllowDefer -DeferTimes 3 -CheckDiskSpace -PersistPrompt
+
+        ## Show Progress Message (with the default message)
+        Show-InstallationProgress -WindowLocation 'TopCenter'
+
+        ## <Perform Pre-Installation tasks here>
+
+        ##*===============================================
+        ##* INSTALLATION
+        ##*===============================================
+        [String]$installPhase = 'Installation'
+
+        ## Handle Zero-Config MSI Installations
+        If ($useDefaultMsi) {
+            [Hashtable]$ExecuteDefaultMSISplat = @{ Action = 'Install'; Path = $defaultMsiFile }; If ($defaultMstFile) {
+                $ExecuteDefaultMSISplat.Add('Transform', $defaultMstFile)
+            }
+            Execute-MSI @ExecuteDefaultMSISplat; If ($defaultMspFiles) {
+                $defaultMspFiles | ForEach-Object { Execute-MSI -Action 'Patch' -Path $_ }
+            }
+        }
+
+        ## <Perform Installation tasks here>
+		
+		#Create C:\Program Files\Fiserv Drive Mapper if it doesn't exist
+		if(!(Test-Path "C:\Program Files\Fiserv Drive Mapper")){
+			New-Folder -Path "C:\Program Files\Fiserv Drive Mapper" -ContinueOnError $false
+			Write-Log -Message "Fiserv Drive Mapper folder created successfully..."
+		}
+		
+		#Create start menu folder if it doesn't exist
+		if(!(Test-Path -Path "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Fiserv Drive Mapper")){
+			New-Folder -Path "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Fiserv Drive Mapper" -ContinueOnError $false
+			Write-Log -Message "Start Menu folder created successfully..."
+		}
+		
+		Copy-File -Path "$dirFiles\DriveMapping.exe" -Destination "C:\Program Files\Fiserv Drive Mapper" -ContinueOnError $false
+		
+		New-Shortcut -Path "$envProgramData\Microsoft\Windows\Start Menu\Programs\Fiserv Drive Mapper\Fiserv Drive Mapper 1.0.lnk" -TargetPath "C:\Program Files\Fiserv Drive Mapper\DriveMapping.exe" -IconLocation "C:\Program Files\Fiserv Drive Mapper\DriveMapping.exe" -Description 'Fiserv Drive Mapper' -WorkingDirectory "C:\Program Files\Fiserv Drive Mapper" -ContinueOnError $false
+		
+		$Path = "C:\users\*\"
+
+		Get-ChildItem $Path | ForEach-Object {Copy-Item "$dirFiles\NetworkDrives.txt" $_ -Force}
+		
+		Copy-File -Path "$dirFiles\uninstall.cmd" -Destination "C:\Program Files\Fiserv Drive Mapper" -ContinueOnError $false
+
+        ##*===============================================
+        ##* POST-INSTALLATION
+        ##*===============================================
+        [String]$installPhase = 'Post-Installation'
+
+        ## <Perform Post-Installation tasks here>
+		
+		#Create Fiserv Drive Mapper UninstallKey
+		$InstallSource = Get-Location
+		$InstallDate = Get-Date -Format 'yyyyMMdd'
+		$DisplayName = "Fiserv Drive Mapper"
+		$DisplayVersion = "1.0"
+		$InstallLocation = "C:\Program Files\Fiserv Drive Mapper"
+		$Publisher = "Fiserv"
+		$UninstallString = "C:\Program Files\Fiserv Drive Mapper\uninstall.cmd"
+		
+		New-Item -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\FiservDriveMapper -Force
+			
+		New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\FiservDriveMapper -Name "DisplayName" -Value $DisplayName -Force
+		New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\FiservDriveMapper -Name "DisplayVersion" -Value $DisplayVersion -Force
+		New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\FiservDriveMapper -Name "InstallLocation" -Value $InstallLocation -Force
+		New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\FiservDriveMapper -Name "Publisher" -Value $Publisher -Force
+		New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\FiservDriveMapper -Name "InstallSource" -Value $InstallSource -Force
+		New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\FiservDriveMapper -Name "InstallDate" -Value $InstallDate -Force
+		New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\FiservDriveMapper -Name "UninstallString" -Value $UninstallString -Force
+		New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\FiservDriveMapper -Name "NoModify" -PropertyType DWord -Value "1" -Force
+		New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\FiservDriveMapper -Name "NoRepair" -PropertyType DWord -Value "1" -Force
+
+        #Hide from Programs and Features
+        New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\FiservDriveMapper -Name "SystemComponent" -PropertyType DWord -Value "1" -Force
+		
+    }
+    ElseIf ($deploymentType -ieq 'Uninstall') {
+        ##*===============================================
+        ##* PRE-UNINSTALLATION
+        ##*===============================================
+        [String]$installPhase = 'Pre-Uninstallation'
+
+        ## Show Welcome Message, close Internet Explorer with a 60 second countdown before automatically closing
+        #Show-InstallationWelcome -CloseApps 'iexplore' -CloseAppsCountdown 60
+
+        ## Show Progress Message (with the default message)
+        Show-InstallationProgress -WindowLocation 'TopCenter'
+
+        ## <Perform Pre-Uninstallation tasks here>
+		
+
+        ##*===============================================
+        ##* UNINSTALLATION
+        ##*===============================================
+        [String]$installPhase = 'Uninstallation'
+
+        ## Handle Zero-Config MSI Uninstallations
+        If ($useDefaultMsi) {
+            [Hashtable]$ExecuteDefaultMSISplat = @{ Action = 'Uninstall'; Path = $defaultMsiFile }; If ($defaultMstFile) {
+                $ExecuteDefaultMSISplat.Add('Transform', $defaultMstFile)
+            }
+            Execute-MSI @ExecuteDefaultMSISplat
+        }
+
+        ## <Perform Uninstallation tasks here>
+		
+		Remove-Folder -Path "C:\Program Files\Fiserv Drive Mapper"
+		
+		Remove-Folder -Path "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Fiserv Drive Mapper"
+		
+		#Check for and delete uninstall key
+		If (Test-Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\FiservDriveMapper) {
+                Try {
+                    Remove-RegistryKey -Key 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\FiservDriveMapper' -ErrorAction Stop
+					Write-Log "FiservDriveMapper Uninstall Key has been deleted successfully"
+                }
+                Catch {
+                    Write-Log -Message "Failed to remove HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\FiservDriveMapper"
+                }
+        }
+        
+        ##*===============================================
+        ##* POST-UNINSTALLATION
+        ##*===============================================
+        [String]$installPhase = 'Post-Uninstallation'
+
+        ## <Perform Post-Uninstallation tasks here>
+
+
+    }
+    ElseIf ($deploymentType -ieq 'Repair') {
+        ##*===============================================
+        ##* PRE-REPAIR
+        ##*===============================================
+        [String]$installPhase = 'Pre-Repair'
+
+        ## Show Welcome Message, close Internet Explorer with a 60 second countdown before automatically closing
+        #Show-InstallationWelcome -CloseApps 'iexplore' -CloseAppsCountdown 60
+
+        ## Show Progress Message (with the default message)
+        Show-InstallationProgress -WindowLocation 'TopCenter'
+
+        ## <Perform Pre-Repair tasks here>
+
+        ##*===============================================
+        ##* REPAIR
+        ##*===============================================
+        [String]$installPhase = 'Repair'
+
+        ## Handle Zero-Config MSI Repairs
+        If ($useDefaultMsi) {
+            [Hashtable]$ExecuteDefaultMSISplat = @{ Action = 'Repair'; Path = $defaultMsiFile; }; If ($defaultMstFile) {
+                $ExecuteDefaultMSISplat.Add('Transform', $defaultMstFile)
+            }
+            Execute-MSI @ExecuteDefaultMSISplat
+        }
+        ## <Perform Repair tasks here>
+
+
+        ##*===============================================
+        ##* POST-REPAIR
+        ##*===============================================
+        [String]$installPhase = 'Post-Repair'
+
+        ## <Perform Post-Repair tasks here>
+
+
+    }
+    ##*===============================================
+    ##* END SCRIPT BODY
+    ##*===============================================
+
+    ## Call the Exit-Script function to perform final cleanup operations
+    Exit-Script -ExitCode $mainExitCode
+}
+Catch {
+    [Int32]$mainExitCode = 60001
+    [String]$mainErrorMessage = "$(Resolve-Error)"
+    Write-Log -Message $mainErrorMessage -Severity 3 -Source $deployAppScriptFriendlyName
+    #Show-DialogBox -Text $mainErrorMessage -Icon 'Stop'
+    Exit-Script -ExitCode $mainExitCode
+}
