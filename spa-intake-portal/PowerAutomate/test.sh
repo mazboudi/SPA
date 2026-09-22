93cd008c497d628919a69f28056bb7416386de0e
F7NXPWL <dean.mazboudi@fiserv.com>
2026-09-22 13:56:20 -0400

    cicd details

--- spa-intake-portal/PowerAutomate/test.sh
+++ spa-intake-portal/PowerAutomate/test.sh
@@ -1,4 +1,4 @@
-curl -X POST "<YOUR_COPIED_URL>" \
+curl -X POST "<https://9b15756c6443e7bd819c018a44fb28.12.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/29/workflows/330c841f8f304c31891c73d8c87c31f8/triggers/manual/paths/invoke?api-version=1>" \
   -H "Content-Type: application/json" \
   -d '{
     "requestId": "REQ-101",
